5edc4a7fe4c1d7b4aad712a778ed38b9102a2bf6
HalilBeyAbim <khalilbaygkh@code.edu.az>
2022-07-01 17:49:14 +0400

    first commit

---
+++ assets/JS/app.js
@@ -0,0 +1,7 @@
+//Area of the Circle
+var radius = 10
+alert('dairenin sahesi: '+ (Math.max(Math.PI)*Math.pow(radius,2)));
+
+// Area of the Square
+var length = 8
+alert('kvadratin sahesi: ' + Math.pow(length,2));
